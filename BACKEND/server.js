56095c89d93bc5467cbb0c36aba9cc9b2ecc24d0
devinbi <it20062088@my.sliit.lk>
2023-03-22 01:00:48 +0530

    initial structure

---
+++ BACKEND/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors({origin: true, credentials: true}));
+app.use(express.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+
+const connection = mongoose.connection;
+//connect database
+ connection.once("open",() => {
+    console.log("mongodb Connection Sucess !");
+ })
+
+
+const allocatingRouter = require("./routes/allocatings.js");
+
+
+app.use("/allocating",allocatingRouter);
+
+const equipmentRouter = require("./routes/equipments.js");
+
+app.use("/equipment",equipmentRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`server is up and running on porT: ${PORT}`);
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+// import  AddAllocating  from "./components/equipmentManagement/Addallocating";
+// import AddEquipment from "./components/equipmentManagement/Addequipment";
+
+
+// function App() {
+
+
+//   return ( 
+//   <div className="App">
+//       <AddAllocating/>
+      
+//     </div>
+//   )
+ 
+// }
+
+// export default App;
