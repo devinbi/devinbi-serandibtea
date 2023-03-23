09a09f8013784a2c096fc1d5c1d16091babb158d
devinbi <it20062088@my.sliit.lk>
2023-03-23 12:08:23 +0530

    feat: Add_data_update_functionality_of_equipment_to_backend

--- BACKEND/routes/equipments.js
+++ BACKEND/routes/equipments.js
@@ -29,65 +29,66 @@ http: router.route("/add").post((req, res) => {
       console.log(err);
     });
 
-  //})
-  // //fetch
+  })
 
-  // http://Localhost:8070/expense
-  // router.route("/").get((req,res)=>{
-
-  //     expense.find().then((expenses)=>{
-  //         res.json(expenses)
-  //     }).catch((err)=>{
-  //         console.log(err)
-  //     })
-  // })
-  // //update
-
-  // http://Localhost:8070/expense/update
-  // //async function (runs simaltaneously)
-  // //fetching userid
-  // router.route("/update/:id").put(async (req, res) => {
-  //     let userid = req.params.id;
-  //     const{ equipmentid,equipmentname,equipmenttype,date,department,intertek,status} = req.body;
-
-  //     const updateexpense = {
-  //         equipmentid,
-  //         equipmentname,
-  //         equipmenttype,
-  //         date,
-  //         department,
-  //         intertek,
-
-  //     }
-  //     //waiting till update is over
-  //     const update = await expense.findByIdAndUpdate(userid,updateexpense).then(() => {
-  //         res.status(200).send({status:"user updated"})
-  //     }).catch((err)=>{
-  //     res.status(500).send({status:"error with updating data" ,error: err.message});
-  //   })
-
-  // })
+  //fetch
+  
+  
+  router.route("/").get((req,res)=>{
+  
+      equipment.find().then((equipment)=>{
+          res.json(equipment)
+      }).catch((err)=>{
+          console.log(err)
+      })
+  })
+  //update
+  
+  
+  //async function (runs simaltaneously)
+  //fetching userid 
+  router.route("/update/:id").put(async (req, res) => {
+      let userid = req.params.id;
+      const{ equipmentid,equipmentname,equipmenttype,date,department,intertek,status} = req.body;
+  
+      const updateequipment = {
+          equipmentid,
+          equipmentname,
+          equipmenttype,
+          date,
+          department,
+          intertek,
+          
+  
+      }
+      //waiting till update is over
+      const update = await equipment.findByIdAndUpdate(userid,updateequipment).then(() => {
+          res.status(200).send({status:"user updated"})
+      }).catch((err)=>{
+      res.status(500).send({status:"error with updating data" ,error: err.message});
+    })
+     
+  })
   // //delete
-
-  // http://Localhost:8070/expense/delete
+  
+  
   // router.route("/delete/:id").delete(async(req,res)=>{
   //   let userid=req.params.id;
-  //   await expense.findByIdAndDelete(userid).then(()=>{
+  //   await equipment.findByIdAndDelete(userid).then(()=>{
   //       res.status(200).send({status:"user deleted"});
   //   }).catch((err)=>{
   //       console.log(err.message);
   //       res.status(500).send({status:"error with delete user", error:err.message});
   //   })
-
+    
   // })
-
-  // router.route("/get/:id").get(async (req,res) =>{
-  //     let userid = req.params.id;
-  //     await expense.findById(userid).then(()=>{
-  //         res.status(200).send({status:"user fetched"})
-  //         console.log(err.message);
-  //         res.status(500).send({status:"error with get user",error: err.message});
-  //     })
-});
-
+  
+  router.route("/get/:id").get(async (req,res) =>{
+      let userid = req.params.id;
+      await equipment.findById(userid).then(()=>{
+          res.status(200).send({status:"user fetched"})
+          console.log(err.message);
+          res.status(500).send({status:"error with get user",error: err.message});
+      })
+  })
 module.exports = router;
